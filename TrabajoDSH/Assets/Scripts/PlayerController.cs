903b67fd38d33e885d6b7323f8f0ec3d49f5526e
Marcos Morales Márquez <marcos.moralesm@hotmail.com>
2023-05-04 17:05:28 +0200

    Spawn, punto de caida, trigger para cambiar de nivel
    -En el script del jugador, una variable publica, spawn, se encarga de designar la posición donde se moverá el jugador al caerse del mapa o perder una vida
    -puntoCaida se encarga de recoger la posicion Y del objeto asignado a la variable pública, si el jugador cae por debajo de esta posicion Y, pierde salud y se mueve a spawn
    -Trigger para cambiar de nivel.
    -Retocada algunas cosas del código, añadido comentarios, etc
    
    Nota: todo esto esta por probar y finalizar, ya que lo he hecho en poco tiempo en la universidad entre clases

--- TrabajoDSH/Assets/Scripts/PlayerController.cs
+++ TrabajoDSH/Assets/Scripts/PlayerController.cs
@@ -14,11 +14,13 @@ public class PlayerController : MonoBehaviour
 
     // Movimiento horizontal:
     Vector2 horizontalInput;
+    [Tooltip("Controlador del jugador")]
     [SerializeField] CharacterController controller;
     [Tooltip("Velocidad del jugador")]
     [SerializeField] float velocidad = 11.0f;
 
     // Movimiento vertical:
+    [Tooltip("Gravedad del jugador")]
     [SerializeField] float gravedad = -30.0f;
     Vector3 verticalVel = Vector3.zero;
     [Tooltip("Layer que el script detecta como suelo para poder saltar")]
@@ -26,6 +28,7 @@ public class PlayerController : MonoBehaviour
     bool isGrounded;
 
     // Salto:
+    [Tooltip("Altura del salto que realiza el jugador")]
     [SerializeField] float alturaSalto = 3.5f;
     bool jump;
 
@@ -33,7 +36,7 @@ public class PlayerController : MonoBehaviour
     [Tooltip("Camara asignada al jugador")]
     [SerializeField] Camera cam;
 
-    // Salud del jugador:
+    // Salud del jugador, 2 al inicio:
     int salud = 2;
 
     // Vidas del jugador, no es lo mismo que salud, si la salud llega a 0, se reduce una vida y salud = 2:
@@ -41,6 +44,12 @@ public class PlayerController : MonoBehaviour
 
     // Puntuación del jugador:
     int puntuacion = 0;
+
+    [Tooltip("Posición donde se moverá el jugador al perder vida/caer")]
+    [SerializeField] Transform spawn;
+
+    [Tooltip("Posición donde el jugador será movido a spawn al caer debajo de la posicion Y de puntoCaida")]
+    [SerializeField] Transform puntoCaida;
 
     #endregion
 
@@ -91,17 +100,32 @@ public class PlayerController : MonoBehaviour
         {
             salud = 2;
             vidas--;
+            
+            // Mover el jugador a la posicion de spawn
+            transform.position = spawn.position; 
         }
         else if (salud == 0 && vidas == 0)
         {
             //TODO Has perdido, cargar escena de derrota
         }
 
-        //TODO Reducir tamaño jugador si salud pasa de 2 a 1? Usar una funcion mejor
+        //TODO Reducir tamaño jugador si salud pasa de 2 a 1? Usar una funcion mejor, que reciba si crece o aumenta, y modificar escala Y jugador
         //TODO Cambiar modelo de fuego a normal al perder salud, usar funcion
 
-        //TODO Flor de Fuego:
-        // if (salud == 3)
+        //TODO Flor de Fuego, solo cuando salud == 3, el jugador puede disparar bolas de fuego
+        /*if (salud == 3)
+        {
+            // Código para comprobar si se ha pulsado keybinding para disparar
+            // Disparar la bola de fuego, que se borre al cabo de x tiempo
+            // Si la bola de fuego da a un enemigo, eliminarlo o hacerle daño, script en la bola que compruebe el tag
+        }*/
+
+        // Si se cae del nivel (debajo de Y de puntoCaida), mover a spawn y perder salud
+        if (transform.position.y < puntoCaida.position.y)
+        {
+            transform.position = spawn.position;
+            salud--;
+        }
     }
 
     void Awake()
@@ -115,6 +139,7 @@ public class PlayerController : MonoBehaviour
         jugador.Movimiento.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
         // Saltar, llama a la funcion OnJumpPressed, que asigna jump a true:
         jugador.Saltar.performed += _ => OnJumpPressed();
+        //TODO Disparar bola de fuego:
     }
 
     void OnEnable()
@@ -159,6 +184,7 @@ public class PlayerController : MonoBehaviour
             salud = 3;
 
             //TODO Cambiar modelo
+            //TODO aumentar tamaño si salud == 1, llamar funcion para cambiar escala Y
 
             Debug.Log("Has cogido una flor de fuego! Salud: " + salud);
             Destroy(other.gameObject);
@@ -170,7 +196,7 @@ public class PlayerController : MonoBehaviour
             {
                 salud = 2;
                 
-                //TODO Aumentar tamaño jugador?
+                //TODO Aumentar tamaño jugador? llamar funcion para cambiar escala Y
             }
             else
             {
@@ -184,6 +210,11 @@ public class PlayerController : MonoBehaviour
             puntuacion += 10;
             Debug.Log("Has cogido una moneda! Puntuacion: " + puntuacion);
             Destroy(other.gameObject);
+        }
+        else if (other.gameObject.tag == "CambiarNivel")
+        {
+            puntuacion += 100;
+            //TODO Cambiar nivel, scenemanagment, pedir nivel como variable publica?
         }
     }
 }
