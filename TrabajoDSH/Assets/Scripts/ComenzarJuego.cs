ee0577705eaec3b6a531656de7cab1824af63b61
ivanoliva01 <ivan.olivalinares@alum.uca.es>
2023-05-12 12:27:51 +0200

    Modificaciones de Menu
    Implementación de los botones
    -COMENZAR JUEGO
    -Nivel1 para ir directamente al juego libre de Nivel1
    -Nivel2 para ir directamente al juego libre de Nivel2
    -Nivel3 para ir directamente al juego libre de Nivel3
    -Nivel4 para ir directamente al juego libre de Nivel4
    -Fondo Nivel 1 y 2
    -Música Nivel 1 y 2

---
+++ TrabajoDSH/Assets/Scripts/ComenzarJuego.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ComenzarJuego : MonoBehaviour
+{
+    [SerializeField] Button btnComenzarJuego;
+    [SerializeField] Button btnNivel1;
+    [SerializeField] Button btnNivel2;
+    [SerializeField] Button btnNivel3;
+    [SerializeField] Button btnNivel4;
+
+
+    private void Start()
+    {
+        btnComenzarJuego.onClick.AddListener(IrANivel1);
+        btnNivel1.onClick.AddListener(IrANivel1);
+        btnNivel2.onClick.AddListener(IrANivel2);
+        btnNivel3.onClick.AddListener(IrANivel3);
+        btnNivel4.onClick.AddListener(IrANivel4);
+    }
+
+    private void IrANivel1()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(1);
+    }
+        private void IrANivel2()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(2);
+    }
+        private void IrANivel3()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(3);
+    }
+        private void IrANivel4()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(4);
+    }
+    
+}
