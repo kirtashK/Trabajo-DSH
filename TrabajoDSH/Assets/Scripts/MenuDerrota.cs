3cb3ef48cb8d9458cb94fd049ada74f06cb501bc
CharbelBm13 <charbelboueri123@gmail.com>
2023-05-17 19:09:25 +0200

    Fin Nivel 2, enemigos nivel 1, escenas victoria y derrota

---
+++ TrabajoDSH/Assets/Scripts/MenuDerrota.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuDerrota : MonoBehaviour
+{
+    [SerializeField] Button btnMenu;
+    [SerializeField] Button btnNivel1;
+
+
+    private void Start()
+    {
+
+        btnNivel1.onClick.AddListener(IrANivel1);
+        btnMenu.onClick.AddListener(IrAMenu);
+
+    }
+
+    private void IrANivel1()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(1);
+    }
+        private void IrAMenu()
+    {
+        Debug.Log("cambio de escena");
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
